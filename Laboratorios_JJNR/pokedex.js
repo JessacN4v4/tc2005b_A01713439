9eeead74d838d184ae7ea33d02a0ec41bfccb2fd
JessacN4v4 <a01713439@tec.mx>
2026-03-11 00:33:30 -0600

    Laboratorio 11 Express

--- Laboratorios_JJNR/pokedex.js
+++ Laboratorios_JJNR/pokedex.js
@@ -12,25 +12,49 @@ const catalogo = document.querySelectorAll(".poke-item");
 
 let equipo = Array(6).fill(null);
 
+// funcion para activar/desactivar boton detalla de equipo
+function actualizarBotonDetalle() {
+    const slots = document.querySelectorAll(".slot");
+    const btn =document.getElementById("btn-detalle");
+
+    const lleno = [...slots].every(s => !s.textContent.includes("Vacío"));
+
+    if (btn) {
+        if (lleno) {
+            btn.classList.remove("bg-gray-400", "text-gray-700", "cursor-not-allowed");
+            btn.classList.add("bg-purple-600", "text-white", "hover:bg-purple-700", "cursor-pointer");
+            btn.href = "/equipo/detalle";
+
+            enviarEquipoSiEstaLleno(); // ← SOLO AQUÍ SE ENVÍA
+        } else {
+            btn.classList.add("bg-gray-400", "text-gray-700", "cursor-not-allowed");
+            btn.classList.remove("bg-purple-600", "text-white", "hover:bg-purple-700", "cursor-pointer");
+            btn.href = "#";
+        }
+    }
+}
+
 // agregar al equipo
 catalogo.forEach(item => {
     item.addEventListener("click", () => {
-        const nombre = item.dataset.nombre;
+    const nombre = item.dataset.nombre;
+    const slotLibre = equipo.indexOf(null);
+    if (slotLibre === -1) return;
 
-        const slotLibre = equipo.indexOf(null);
-        if (slotLibre === -1) return;
+    equipo[slotLibre] = nombre;
 
-        equipo[slotLibre] = nombre;
+    slots[slotLibre].innerHTML = `
+        <div class="text-center">
+            <img src="${item.querySelector("img").src}" class="w-16 mx-auto">
+            <p class="font-bold">${nombre}</p>
+        </div>
+    `;
 
-        slots[slotLibre].innerHTML = `
-            <div class="text-center">
-                <img src="${item.querySelector("img").src}" class="w-16 mx-auto">
-                <p class="font-bold">${nombre}</p>
-            </div>
-        `;
+    item.style.display = "none";
 
-        item.style.display = "none";
-    });
+    actualizarBotonDetalle(); 
+});
+
 });
 
 // quitar del equipo
@@ -44,5 +68,21 @@ slots.forEach((slot, index) => {
         slot.innerHTML = "Vacío";
 
         document.querySelector(`[data-nombre="${nombre}"]`).style.display = "block";
+
+        actualizarBotonDetalle();
     });
 });
+
+// funcion que manda el equipo selecionado solo si estan llenos los slots
+function enviarEquipoSiEstaLleno() {
+    const lleno = equipo.every(x => x !== null);
+
+    if (lleno) {
+        fetch('/equipo/actualizar', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ equipo })
+        });
+    }
+}
+
---
+++ Laboratorios_JJNR/routes/pokemons.data.js
@@ -0,0 +1,60 @@
+const pokemons = [
+    {
+        nombre: "Umbreon",
+        descripcion: "Si se expone al aura de la luna, los anillos de su cuerpo relucen.",
+        tipo: "Siniestro",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/197.png",
+        debilidades: "Bicho, Hada, Lucha",
+        fortalezas: "Fantasma, Psíquico"
+    },
+    {
+        nombre: "Sableye",
+        descripcion: "Hace su guarida en cuevas oscuras y come gemas.",
+        tipo: "Siniestro/Fantasma",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/302.png",
+        debilidades: "Hada",
+        fortalezas: "Fantasma, Psíquico"
+    },
+    {
+        nombre: "Dratini",
+        descripcion: "Muda muchas veces de piel mientras crece.",
+        tipo: "Dragón",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/147.png",
+        debilidades: "Dragón, Hada, Hielo",
+        fortalezas: "Dragón"
+    },
+    {
+        nombre: "Gengar",
+        descripcion: "Se oculta en las sombras y absorbe el calor de sus víctimas.",
+        tipo: "Fantasma/Veneno",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/094.png",
+        debilidades: "Tierra, Fantasma, Siniestro",
+        fortalezas: "Hada, Fantasma, Planta, Psíquico"
+    },
+    {
+        nombre: "Lycanroc",
+        descripcion: "Ataca con colmillos y garras afiladas.",
+        tipo: "Roca",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/745.png",
+        debilidades: "Tierra, Lucha, Planta, Agua, Hierro",
+        fortalezas: "Fuego, Hielo, Volador, Bicho"
+    },
+    {
+        nombre: "Gible",
+        descripcion: "Permanece oculto en cuevas y muerde con fuerza.",
+        tipo: "Dragón/Tierra",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/443.png",
+        debilidades: "Hielo, Hada, Dragón",
+        fortalezas: "Dragón, Roca, Fuego, Eléctrico, Veneno, Hierro"
+    },
+    {
+        nombre: "Yamper",
+        descripcion: "Genera electricidad al correr.",
+        tipo: "Eléctrico",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/835.png",
+        debilidades: "Tierra",
+        fortalezas: "Agua, Volador"
+    }
+];
+
+module.exports = pokemons;
